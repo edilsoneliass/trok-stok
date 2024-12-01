fc586a22a84c723b052109176e4789816712ec73
Edilson Elias <edilsonbgd@gmail.com>
2024-12-01 18:27:33 -0300

    Resolver bug de cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,6 +59,12 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+]
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
